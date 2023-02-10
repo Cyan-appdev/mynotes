319b8283578cd2b31b66b1bdaeebca2e2e864f5f
Cyangrey <xtianity9@gmail.com>
2023-02-10 21:07:00 +0400

    Step 25

--- lib/services/auth/bloc/auth_state.dart
+++ lib/services/auth/bloc/auth_state.dart
@@ -4,35 +4,56 @@ import 'package:equatable/equatable.dart';
 
 @immutable
 abstract class AuthState {
-  const AuthState();
+  final bool isLoading;
+  final String? loadingText;
+  const AuthState({
+    required this.isLoading,
+    this.loadingText = 'Please wait a moment',
+  });
 }
 
 class AuthStateUninitialized extends AuthState {
-  const AuthStateUninitialized();
-}
-
-class AuthStateLoggedIn extends AuthState {
-  final AuthUser user;
-  const AuthStateLoggedIn(this.user);
-}
-
-class AuthStateNeedsVerification extends AuthState {
-  const AuthStateNeedsVerification();
-}
-
-class AuthStateLoggedOut extends AuthState with EquatableMixin {
-  final Exception? exception;
-  final bool isLoading;
-  const AuthStateLoggedOut({
-    required this.exception,
-    required this.isLoading,
-  });
-
-  @override
-  List<Object?> get props => [exception, isLoading];
+  const AuthStateUninitialized({
+    required bool isLoading,
+  }) : super(isLoading: isLoading);
 }
 
 class AuthStateRegistering extends AuthState {
   final Exception exception;
-  const AuthStateRegistering(this.exception);
+  const AuthStateRegistering({
+    required this.exception,
+    required bool isLoading,
+  }) : super(isLoading: isLoading);
+}
+
+class AuthStateLoggedIn extends AuthState {
+  final AuthUser user;
+  const AuthStateLoggedIn({
+    required this.user,
+    required bool isLoading,
+  }) : super(isLoading: isLoading);
+}
+
+class AuthStateNeedsVerification extends AuthState {
+  const AuthStateNeedsVerification({
+    required bool isLoading,
+  }) : super(isLoading: isLoading);
+}
+
+class AuthStateLoggedOut extends AuthState with EquatableMixin {
+  final Exception? exception;
+  const AuthStateLoggedOut({
+    required this.exception,
+    required bool isLoading,
+    String? loadingText,
+  }) : super(
+          isLoading: isLoading,
+          loadingText: loadingText,
+        );
+
+  @override
+  List<Object?> get props => [
+        exception,
+        isLoading,
+      ];
 }
